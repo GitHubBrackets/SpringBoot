03c6f3dad6b8bc536b4162fabc308636e88d7331
GitHubBrackets <diego.bolanios@gmail.com>
2022-01-12 15:12:57 -0600

    first commit

---
+++ src/main/java/com/dd/art/ArtefactDiegoApplication.java
@@ -0,0 +1,13 @@
+package com.dd.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtefactDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtefactDiegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dd/art/ArtefactDiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtefactDiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
